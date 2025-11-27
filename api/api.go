617c33d0d47f6a1fdfc660f42d4fda80e79102c6
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2025-11-27 13:47:47 -0300

    feat: adding log system

--- api/api.go
+++ api/api.go
@@ -5,16 +5,36 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/rafaeldepontes/auth-go/configs"
 	"github.com/rafaeldepontes/auth-go/internal/database"
+	"github.com/rafaeldepontes/auth-go/internal/domain"
 	"github.com/rafaeldepontes/auth-go/internal/repository"
 	"github.com/rafaeldepontes/auth-go/internal/service"
+	log "github.com/sirupsen/logrus"
+	prefixed "github.com/x-cray/logrus-prefixed-formatter"
 )
 
+func initLogger() *log.Logger {
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetOutput(os.Stdout)
+	return &log.Logger{
+		Out:   os.Stderr,
+		Level: log.DebugLevel,
+		Formatter: &prefixed.TextFormatter{
+			DisableColors:   true,
+			TimestampFormat: "2006-01-02 15:04:05",
+			FullTimestamp:   true,
+			ForceFormatting: true,
+		},
+	}
+}
+
 // Init initialize all the resources needed for the server run properly
-func Init() (*Configuration, *Application, *sql.DB, error) {
+func Init() (*configs.Configuration, *domain.Application, *sql.DB, error) {
+	var logger *log.Logger = initLogger()
 	godotenv.Load(".env", ".env.example")
 
-	config := &Configuration{
+	config := &configs.Configuration{
 		JwtBasedPort:        os.Getenv("JWT_PORT"),
 		CookieBasedPort:     os.Getenv("COOKIE_PORT"),
 		JwtRefreshBasedPort: os.Getenv("JWT_REFRESH_PORT"),
@@ -23,10 +43,11 @@ func Init() (*Configuration, *Application, *sql.DB, error) {
 	db, err := database.Open()
 
 	var userRepository *repository.UserRepository = repository.NewUserRepository(db)
-	var userService *service.UserService = service.NewUserService(userRepository)
+	var userService *service.UserService = service.NewUserService(userRepository, logger)
 
-	application := &Application{
+	application := &domain.Application{
 		UserService: userService,
+		Logger:      logger,
 	}
 
 	return config, application, db, err
