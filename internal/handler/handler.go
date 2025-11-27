a2ac1071259060fedbbcd79f9e2d719fe36e6448
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2025-11-27 13:51:10 -0300

    fix: fixing handler, wrong package import

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -3,15 +3,15 @@ package handler
 import (
 	"github.com/go-chi/chi/v5"
 	chimiddleware "github.com/go-chi/chi/v5/middleware"
-	"github.com/rafaeldepontes/auth-go/api"
+	"github.com/rafaeldepontes/auth-go/internal/domain"
 )
 
 // Handler controls the system routes based on *chi.Mux and a configuration struct.
-func Handler(r *chi.Mux, app *api.Application) {
+func Handler(r *chi.Mux, app *domain.Application) {
 	r.Use(chimiddleware.StripSlashes)
 
 	// Public
-	r.Route("api/v1", func(r chi.Router) {
+	r.Route("/api/v1", func(r chi.Router) {
 		r.Get("/users", app.UserService.FindAllUsers)
 	})
 
