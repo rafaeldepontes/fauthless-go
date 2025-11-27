6ee2ad9ca4a1906ce6d6a5794ad8dca60fe42830
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2025-11-27 11:14:20 -0300

    fix: adding the same thing for each auth main

--- cmd/jwt-based/main.go
+++ cmd/jwt-based/main.go
@@ -4,12 +4,24 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/rafaeldepontes/auth-go/api"
+	"github.com/rafaeldepontes/auth-go/internal/handler"
 )
 
 func main() {
-	config := api.Init()
-	
+	var app *api.Application
+	var config *api.Configuration
+
+	config, app, db, err := api.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	var r *chi.Mux = chi.NewRouter()
+	handler.Handler(r, app)
+
 	fmt.Printf("API running at %v\n", config.JwtBasedPort)
 
 	http.ListenAndServe(config.JwtBasedPort, nil)
--- cmd/jwt-refresh-based/main.go
+++ cmd/jwt-refresh-based/main.go
@@ -4,11 +4,23 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/rafaeldepontes/auth-go/api"
+	"github.com/rafaeldepontes/auth-go/internal/handler"
 )
 
 func main() {
-	config := api.Init()
+	var app *api.Application
+	var config *api.Configuration
+
+	config, app, db, err := api.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	var r *chi.Mux = chi.NewRouter()
+	handler.Handler(r, app)
 
 	fmt.Printf("API running at %v\n", config.JwtRefreshBasedPort)
 
