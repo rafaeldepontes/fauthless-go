5683d26b8924f6ffdccc80829ec36c9eb2f87979
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2025-11-27 13:51:31 -0300

    feat: add the repository call for Users

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -1,9 +1,18 @@
 package repository
 
-import "database/sql"
+import (
+	"database/sql"
+)
 
 type UserRepository struct {
 	db *sql.DB
+}
+
+type User struct {
+	Username       *string `json:"username"`
+	HashedPassword *string `json:"password,omitempty"`
+	Id             *uint   `json:"id,omitempty"`
+	Age            *uint   `json:"age,omitempty"`
 }
 
 // NewUserRepository initialize a new UserRepository containing
@@ -12,4 +21,31 @@ func NewUserRepository(conn *sql.DB) *UserRepository {
 	return &UserRepository{
 		db: conn,
 	}
+}
+
+func (repo *UserRepository) FindAllUsers() ([]User, error) {
+	query := `SELECT id, username, age FROM users;`
+
+	rows, err := repo.db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	usersMemAddress := new([]User)
+	users := *usersMemAddress
+
+	for rows.Next() {
+		var u User
+		if err := rows.Scan(&u.Id, &u.Username, &u.Age); err != nil {
+			return nil, err
+		}
+		users = append(users, u)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return users, nil
 }
