65404fe6d8fd88304ff6d85426ff9db01d2acc8b
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2025-11-28 17:03:32 -0300

    feat: adding pagination protection on the server side

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -3,6 +3,7 @@ package service
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/rafaeldepontes/auth-go/internal/errorhandler"
 	"github.com/rafaeldepontes/auth-go/internal/repository"
@@ -25,19 +26,32 @@ func NewUserService(userRepo *repository.UserRepository, logg *log.Logger) *User
 // FindAllUsers list all the users without a filter and returns each
 // one with pagination and a few datas missing for LGPD
 func (us *UserService) FindAllUsers(w http.ResponseWriter, r *http.Request) {
+	us.Logger.Infoln("Listing all the users in the database...")
 
-	us.Logger.Infoln("[LOG] Listing all the users in the database...")
+	sizeStr := r.URL.Query().Get("size")
+	if sizeStr == "" {
+		sizeStr = "25"
+	}
+	size, _ := strconv.Atoi(sizeStr)
+
+	currentPageStr := r.URL.Query().Get("page")
+	if currentPageStr == "" {
+		currentPageStr = "1"
+	}
+	currentPage, _ := strconv.Atoi(currentPageStr)
+	currentPage-- // If page is the first one, in the URL should be "1" and in the code should be 0 for the offset...
+
 	var users []repository.User
-	users, err := us.userRepository.FindAllUsers()
+	users, err := us.userRepository.FindAllUsers(size, currentPage)
 	if err != nil {
 		errorhandler.BadRequestErrorHandler(w, err, r.URL.Path)
-		log.Errorf("[ERROR] An error occurred: %v", err)
+		log.Errorf("An error occurred: %v", err)
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
-	us.Logger.Infof("[LOG] Found %v users", len(users))
+	us.Logger.Infof("Found %v users", len(users))
 
 	//TODO: IMPLEMENT PAGINATION FOR THIS ENDPOINT
 	json.NewEncoder(w).Encode(users)
