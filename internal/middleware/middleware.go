1354af3b475fbdcb1619fbbd924d9c1cc7e44339
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2025-11-29 03:10:04 -0300

    feat: adding the filter for each request

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -2,11 +2,29 @@ package middleware
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/rafaeldepontes/auth-go/internal/errorhandler"
+	jwt "github.com/rafaeldepontes/auth-go/internal/token"
 )
 
-func AuthCookieBased(next http.Handler) http.Handler {
+type Middleware struct {
+	JwtBuilder *jwt.JwtBuilder
+}
+
+type contextKey string
+
+const TokenContextKey = contextKey("token")
+
+var Token_Prefix = "Bearer "
+
+func NewMiddleware(sk string) *Middleware {
+	return &Middleware{
+		JwtBuilder: jwt.NewJwtBuilder(sk),
+	}
+}
+
+func (m *Middleware) AuthCookieBased(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var sessioToken *http.Cookie
 		sessioToken, err := r.Cookie("session_token")
@@ -14,13 +32,13 @@ func AuthCookieBased(next http.Handler) http.Handler {
 		// I should check if the token is the same for the user...
 		// but I don't want to.
 		if err != nil || sessioToken.Value == "" {
-			errorhandler.UnauthroizedErrorHandler(w)
+			errorhandler.UnauthroizedErrorHandler(w, errorhandler.ErrInvalidToken)
 			return
 		}
 
 		csrfToken := r.Header.Get("X-CSRF-Token")
 		if csrfToken == "" {
-			errorhandler.UnauthroizedErrorHandler(w)
+			errorhandler.UnauthroizedErrorHandler(w, errorhandler.ErrInvalidCSRFToken)
 			return
 		}
 
@@ -28,16 +46,33 @@ func AuthCookieBased(next http.Handler) http.Handler {
 	})
 }
 
-func JwtBased(next http.Handler) http.Handler {
+func (m *Middleware) JwtBased(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		dirtToken := r.Header.Get("Authorization")
+		token := cleanToken(dirtToken)
+
+		if dirtToken == "" || dirtToken == token {
+			errorhandler.UnauthroizedErrorHandler(w, errorhandler.ErrInvalidToken)
+			return
+		}
+
+		_, err := m.JwtBuilder.VerifyToken(token)
+		if err != nil {
+			errorhandler.UnauthroizedErrorHandler(w, err)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (m *Middleware) JwtRefreshBased(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		next.ServeHTTP(w, r)
 	})
 }
 
-func JwtRefreshBased(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-		next.ServeHTTP(w, r)
-	})
+func cleanToken(dirtToken string) string {
+	return strings.TrimPrefix(dirtToken, Token_Prefix)
 }
