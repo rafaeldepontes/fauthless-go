29234e99b2f7bc09435b488c20e1d79bb9df5bd6
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2025-12-02 01:53:15 -0300

    feat: adding the flow for the oauth2 process

--- api/api.go
+++ api/api.go
@@ -5,12 +5,15 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
+
 	"github.com/rafaeldepontes/auth-go/configs"
+	"github.com/rafaeldepontes/auth-go/internal/auth"
 	"github.com/rafaeldepontes/auth-go/internal/database"
 	"github.com/rafaeldepontes/auth-go/internal/database/repository"
 	"github.com/rafaeldepontes/auth-go/internal/middleware"
 	"github.com/rafaeldepontes/auth-go/internal/service"
 	"github.com/rafaeldepontes/auth-go/internal/storage"
+
 	log "github.com/sirupsen/logrus"
 	prefixed "github.com/x-cray/logrus-prefixed-formatter"
 )
@@ -20,6 +23,7 @@ const (
 	CookieBased
 	JwtBased
 	JwtRefreshBased
+	OAuth2
 )
 
 func initLogger() *log.Logger {
@@ -43,11 +47,18 @@ func Init() (*configs.Configuration, *Application, *sql.DB, error) {
 	godotenv.Load(".env", ".env.example")
 
 	config := &configs.Configuration{
-		SecretKey:           os.Getenv("JWT_SECRET_KEY"),
+		JwtSecretKey:        os.Getenv("JWT_SECRET_KEY"),
 		JwtBasedPort:        os.Getenv("JWT_PORT"),
 		CookieBasedPort:     os.Getenv("COOKIE_PORT"),
+		OAuth2Port:          os.Getenv("OAUTH2_PORT"),
 		JwtRefreshBasedPort: os.Getenv("JWT_REFRESH_PORT"),
+		GoogleClientId:      os.Getenv("GOOGLE_CLIENT_ID"),
+		GoogleSecretKey:     os.Getenv("GOOGLE_KEY"),
+		GoogleClientSecret:  os.Getenv("GOOGLE_CLIENT_SECRET"),
+		UrlCallback:         os.Getenv("URL_CALLBACK"),
 	}
+
+	auth.InitOAuth(config)
 
 	db, err := database.Open()
 
@@ -57,9 +68,9 @@ func Init() (*configs.Configuration, *Application, *sql.DB, error) {
 	var sessionRepository *repository.SessionRepository = repository.NewSessionRepository(db)
 
 	var userService *service.UserService = service.NewUserService(userRepository, logger, caches)
-	var authService *service.AuthService = service.NewAuthService(userRepository, sessionRepository, logger, config.SecretKey, caches)
+	var authService *service.AuthService = service.NewAuthService(userRepository, sessionRepository, logger, config.JwtSecretKey, caches)
 
-	var middleware *middleware.Middleware = middleware.NewMiddleware(config.SecretKey, caches)
+	var middleware *middleware.Middleware = middleware.NewMiddleware(config.JwtSecretKey, caches)
 
 	application := &Application{
 		UserService: userService,
