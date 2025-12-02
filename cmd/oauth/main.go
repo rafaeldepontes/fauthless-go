059eb867e4f551e8a4ec261b37fb39923ea1bfec
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2025-12-02 01:53:52 -0300

    feat: add the main for the oauth

---
+++ cmd/oauth/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+
+	"github.com/rafaeldepontes/auth-go/api"
+	"github.com/rafaeldepontes/auth-go/configs"
+	"github.com/rafaeldepontes/auth-go/internal/handler"
+)
+
+func main() {
+	var app *api.Application
+	var config *configs.Configuration
+
+	config, app, db, err := api.Init()
+	if err != nil {
+		app.Logger.Fatalf("An error occurred: %v", err)
+	}
+	defer db.Close()
+
+	var r *chi.Mux = chi.NewRouter()
+	handler.Handler(r, app, api.OAuth2)
+
+	app.Logger.Infof("API running at %v\n", config.OAuth2Port)
+
+	http.ListenAndServe(config.OAuth2Port, r)
+}
