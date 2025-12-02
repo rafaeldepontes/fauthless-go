a70b8aa67c9dc06287f26e4c9b30209c08dc2009
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2025-12-02 11:17:58 -0300

    feat: adding the new cursor func

--- internal/pagination/pagination.go
+++ internal/pagination/pagination.go
@@ -2,19 +2,29 @@ package pagination
 
 import "github.com/rafaeldepontes/auth-go/internal/domain"
 
-// NewPagination accepts a generic T type, a slice of any data, a current number of pages,
-// a total number of records and a size and it will return a Pagination.
-func NewPagination[T any](data []T, currentPage, totalRecords, size uint) *domain.Pagination[T] {
+// NewOffSetPagination accepts a generic T type, a slice of any data, a current number of pages,
+// a total number of records and a size and it will return a OffSetPagination.
+func NewOffSetPagination[T any](data []T, currentPage, totalRecords, size uint) *domain.OffSetPagination[T] {
 	totalPages := totalRecords / size
 	if totalPages <= 0 {
 		totalPages = 1
 	}
 
-	return &domain.Pagination[T]{
+	return &domain.OffSetPagination[T]{
 		Data:         data,
 		CurrentPage:  currentPage,
 		TotalPages:   totalPages,
 		TotalRecords: totalRecords,
 		Size:         size,
+	}
+}
+
+// NewCursorPagination accepts a generic T type, a slice of any data, a size of records per page and
+// the next page being a pointer to the next id in the database and it will return a CursorPagination.
+func NewCursorPagination[T any](data []T, size int, nextCursor int64) *domain.CursorPagination[T] {
+	return &domain.CursorPagination[T]{
+		Data:       data,
+		Size:       size,
+		NextCursor: nextCursor,
 	}
 }
